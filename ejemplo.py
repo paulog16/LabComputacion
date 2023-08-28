b43aebef2cb80752da348f547098d9e54ce23deb
PauloGuidolin <pguidolin0@gmail.com>
2023-08-28 20:37:16 -0300

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,33 @@
+# num1=int(input('ingrese el numero 1'))
+# num2=int(input('ingrese el numero 2'))
+# num3=int(input('ingrese el numero 3'))
+# num4=int(input('ingrese el numero 4'))
+# numMayor=0
+
+# if numMayor<num1:
+#     numMayor=num1
+# if numMayor<num2:
+#     numMayor=num2
+# if numMayor<num3:
+#     numMayor=num3
+# if numMayor<num4:
+#     numMayor=num4
+
+# print(f'el numero mas grande es: {numMayor}')    
+
+num=int(input('ingrese un numero: '))
+if num % 2==0:
+    print('es par')
+else:
+    print('es impar')
+
+num=int(input('ingrese un numero: '))
+nu2=int(input('ingrese un numero2: '))
+if num % num2==0:
+    print('el numero 1 es multiplo del num2')
+else:
+    print('el numero 1 no es multiplo del num2')
+
+    
+
+
